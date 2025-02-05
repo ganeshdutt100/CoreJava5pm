1d1b96b55c191361b0338b35ec5ccc57be889c31
Ganesh Dutt <gdutt2304@gmail.com>
2025-02-05 18:20:24 +0530

    first commit

---
+++ Basic/class1.java
@@ -0,0 +1,22 @@
+package Basic;
+
+public class class1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+//        int a = 20;
+//        int b = 30;
+//        int sum = a + b;
+//        System.out.println(sum);
+//        System.out.println(a);
+//        System.out.println("b");
+
+
+//        int A = 40;
+
+        String firstName = "Ganesh";
+        String lastName = "Dutt";
+        System.out.println(firstName + " " + lastName);
+//        System.out.println(lastName);
+
+    }
+}
---
+++ Basic/class2.java
@@ -0,0 +1,7 @@
+package Basic;
+
+public class class2 {
+    public static void main(String[] args) {
+
+    }
+}
