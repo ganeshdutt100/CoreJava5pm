cd964fc62b344ffac817431bcd56c9a0121075aa
Ganesh Dutt <gdutt2304@gmail.com>
2025-02-20 17:44:36 +0530

    While and Do While Loop

---
+++ Loop/code2.java
@@ -0,0 +1,20 @@
+package Loop;
+
+public class code2 {
+    public static void main(String[] args) {
+
+//        int i = 20;
+//        while(i <= 10){
+//            System.out.println(i);
+//            i++;
+//        }
+
+
+//        int j  = 7;
+//        do{
+//            System.out.println(j);
+//            j++;
+//        }
+//        while(j <= 10);
+    }
+}
