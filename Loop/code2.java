70548a0b8798940684d76ebf4d6cc133e96ebdef
Ganesh Dutt <gdutt2304@gmail.com>
2025-02-20 18:31:15 +0530

    loop Questions

--- Loop/code2.java
+++ Loop/code2.java
@@ -1,7 +1,10 @@
 package Loop;
 
+import java.util.Scanner;
+
 public class code2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
 //        int i = 20;
 //        while(i <= 10){
@@ -16,5 +19,26 @@ public class code2 {
 //            j++;
 //        }
 //        while(j <= 10);
+
+        System.out.println("Enter your Starting Number : ");
+        int i =  scanner.nextInt();
+        System.out.println("Enter your Ending Number : ");
+        int endingnumber = scanner.nextInt();
+        while(i <= endingnumber) {
+           if(i % 2 == 0) {
+               System.out.println("Even : " + i);
+           }
+           else{
+               System.out.println("Odd : " + i);
+
+            }
+            i++;
+        }
+//          int i = 1;
+//        int table = 2;
+//        while(i <= 10){
+//            System.out.println(i * table);
+//            i++;
+//        }
     }
 }
