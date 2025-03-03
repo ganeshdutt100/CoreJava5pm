8b25553b3919c8194a0a5c19ad28af694d065d19
Ganesh Dutt <gdutt2304@gmail.com>
2025-03-03 17:45:42 +0530

    String

---
+++ String/class1.java
@@ -0,0 +1,51 @@
+package String;
+
+public class class1 {
+    public static void main(String[] args) {
+//        String name = "Ganesh Dutt";
+//        String name1 = "Ganesh Dutt";
+//        System.out.println(name  + name1);
+
+//        String name = new String("Ganesh Dutt");
+//        System.out.println(name);
+
+
+        String name = " Raghav ";
+        String name1 = "raghav";
+        String last = "Sharma";
+        System.out.println(name.length());
+        System.out.println(name + " "+  last);
+        System.out.println(name.concat(" ").concat(last));
+        System.out.println(name.substring(3));
+        System.out.println(name.substring(2,5));
+
+        System.out.println(name.charAt(3));
+        System.out.println(name.indexOf('a',2));
+
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.equals(name1));
+        System.out.println(name.equalsIgnoreCase(name1));
+
+        System.out.println(name.startsWith("R"));
+        System.out.println(name.endsWith("v"));
+
+        System.out.println(name.replaceFirst("a", "o"));
+        String newName = name.trim();
+        System.out.println(newName.length());
+
+         String names = "Raghav,Divya,nshu,Rahul,Sparsh";
+         String nameValue[] = names.split(",");
+         for (String name2 : nameValue) {
+             System.out.println(name2);
+         }
+
+//        System.out.println(names.split(", "));
+//        String names[] =  {"Raghav",  "Divyanshu", "Rahul", "Sparsh" };
+//
+//        for (String name2 : names) {
+//            System.out.println(name2.split(","));
+//        }
+
+    }
+}
